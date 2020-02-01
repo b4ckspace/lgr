f43eda46eb24ad8ecedc5f120b065f2b2a992d3d
Andy U <www.github.com@uhsemann.de>
2020-02-01 12:54:00 +0100

    added initial support for item images

--- src/lgr/models.py
+++ src/lgr/models.py
@@ -1,7 +1,9 @@
 from django.utils import timezone
 from django.utils.functional import cached_property
+from django.utils.html import mark_safe
 from django.db import models
 from django.contrib import messages
+from django.conf import settings
 
 from lgr.mixin import BarcodeHistoryMixin
 
@@ -27,9 +29,14 @@ class Item(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(blank=True)
     tags = models.ManyToManyField(Tag, blank=True)
+    image = models.ImageField(upload_to=settings.MEDIA_ITEM_PATH, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    def image_tag(self):
+        return mark_safe('<img src="%s" width="%s" height="%s" />' % (self.image.url, self.image.width, self.image.height))
+    image_tag.short_description = 'Image Preview'
 
 
 class Barcode(BarcodeHistoryMixin, models.Model):
